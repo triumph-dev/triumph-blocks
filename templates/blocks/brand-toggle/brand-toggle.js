c3d0436aa3a853a57ffaa4d1f1e7b1f947a649ad
Aaron Janke <aaronljanke@gmail.com>
2020-09-23 10:52:03 -0500

    Layout Wrapper, CTA, Brand Toggle, Blog List updated

--- templates/blocks/brand-toggle/brand-toggle.js
+++ templates/blocks/brand-toggle/brand-toggle.js
@@ -2,19 +2,49 @@ export default class BrandToggle {
     constructor(element){
         // Bindings
         this.init = this.init.bind(this);
+        this.refreshBrands = this.refreshBrands.bind(this);
+        this.filterComponents = this.filterComponents.bind(this);
 
         // Definitions
         this.element = document.querySelector(element);
+        this.blocks     = Array.prototype.slice.call(document.querySelectorAll(".article-body section[data-background-color]"));
 
         // Calls
         this.init();
     }
 
+    refreshBrands(e) {
+        var brand = e.target.value;
+        var destination = window.location.origin + "?brand=" + brand;
+        window.location.href = destination;
+    }
+
+    filterComponents(e) {
+        var component = e.target.value;
+        this.blocks.forEach((block, i) => {
+            // Show all components if select is reset
+            if(component == "reset") {
+                block.classList.remove("hide");
+            // Hide all components, unless the class matches what was selected
+            } else {
+                block.classList.add("hide");
+                if((block.classList.contains(component) || block.classList.contains("brand-toggle")) && !block.classList.contains('header')) {
+                    block.classList.remove("hide");
+                    if(this.blocks[i-1]) {
+                        this.blocks[i-1].classList.remove("hide"); // show this component's header
+                    }
+                }
+            }
+        });
+    }
+
     init(){
         this.element.onchange = (e) => {
-            var brand = e.target.value;
-            var destination = window.location.origin + "?brand=" + brand;
-            window.location.href = destination;
+            if(this.element.getAttribute("name") == "brands") {
+                this.refreshBrands(e);
+            } else {
+                this.filterComponents(e);
+            }
         }
     }
 }
