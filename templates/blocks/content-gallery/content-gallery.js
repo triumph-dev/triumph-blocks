ad78cdd39091ddd9952d1917ca94bcba7a22aafa
Aaron Janke <aaronljanke@gmail.com>
2020-10-07 09:38:31 -0500

    Fun edits, yo.

--- templates/blocks/content-gallery/content-gallery.js
+++ templates/blocks/content-gallery/content-gallery.js
@@ -2,6 +2,7 @@ export default class Gallery {
 	constructor(element) {
 		this.element = document.querySelector(element);
 		this.block = this.element.parentNode;
+		this.arrows = this.element.querySelector('.tns-controls');
 		this.slides = this.element.querySelectorAll('.slide');
 		this.navigation = this.block.querySelector('.navigation .dots-container');
 		this.childrenCount = this.element.childElementCount;
@@ -18,6 +19,38 @@ export default class Gallery {
 				document.querySelector('.dot.active').classList.remove('active');
 				slide.classList.add('active');
 				dot.classList.add('active');
+			});
+		});
+		this.arrows.querySelectorAll('button').forEach((arrow) => {
+			arrow.addEventListener("click", () => {
+				this.arrows.querySelector('button[data-controls="next"]').classList.remove("inactive");
+				this.arrows.querySelector('button[data-controls="prev"]').classList.remove("inactive");
+
+				var currentSlideNumber = parseInt(document.querySelector('.dot.active').dataset.slide.split('-')[1]);
+				var nextSlide = document.querySelector('.slide[data-slide="slide-'+ (currentSlideNumber + 1) +'"]');
+				var prevSlide = document.querySelector('.slide[data-slide="slide-'+ (currentSlideNumber - 1) +'"]');
+
+				if(arrow.dataset.controls=="next") {
+					if(nextSlide) {
+						document.querySelector('.slide.active').classList.remove('active');
+						document.querySelector('.dot.active').classList.remove('active');
+						nextSlide.classList.add('active');
+						document.querySelector('.dot[data-slide="slide-'+ (currentSlideNumber + 1) +'"]').classList.add("active");
+					}
+					if(!document.querySelector('.slide[data-slide="slide-'+ (currentSlideNumber + 2) +'"]')) {
+						this.arrows.querySelector('button[data-controls="next"]').classList.add("inactive");
+					}
+				} else {
+					if(prevSlide) {
+						document.querySelector('.slide.active').classList.remove('active');
+						document.querySelector('.dot.active').classList.remove('active');
+						prevSlide.classList.add('active');
+						document.querySelector('.dot[data-slide="slide-'+ (currentSlideNumber - 1) +'"]').classList.add("active");
+					}
+					if(!document.querySelector('.slide[data-slide="slide-'+ (currentSlideNumber - 2) +'"]')) {
+						this.arrows.querySelector('button[data-controls="prev"]').classList.add("inactive");
+					}
+				}
 			});
 		});
 	}
