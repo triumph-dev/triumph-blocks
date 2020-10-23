e6bcd8fb8d898aaa6cdbb6ddb714abcb51dc203c
Andrew Hale <hale.adh@gmail.com>
2020-10-23 18:07:55 -0500

    New Menu

--- templates/blocks/menu/menu.js
+++ templates/blocks/menu/menu.js
@@ -2,6 +2,7 @@ export default class Menu {
 	constructor(menu, toggleButton) {
 		this.toggleButton = document.querySelector(toggleButton);
 		this.menu = document.querySelector(menu);
+		this.subnavs = this.menu.querySelectorAll('[data-subnav-toggle]');
 		this.doc = document.documentElement;
 		this.init();
 	}
@@ -16,6 +17,13 @@ export default class Menu {
 		this.doc.dataset.menuState = 'closed';
 	}
 
+	toggleSubnav(toggle) { 
+		let parent = toggle.closest('[data-subnav-toggle]');
+		parent.dataset.subnavToggle == 'closed'
+			? parent.setAttribute('data-subnav-toggle', 'open')
+			: parent.setAttribute('data-subnav-toggle', 'closed');
+	}
+
 	toggle() {
 		this.menu.dataset.menuState == 'closed'
 			? this.menu.setAttribute('data-menu-state', 'open')
@@ -28,6 +36,14 @@ export default class Menu {
 	init() {
 		this.toggleButton.addEventListener('click', () => {
 			this.toggle();
+		});
+
+		this.subnavs.forEach((subnav) => { 
+	
+			let subnavToggle = subnav.querySelector('.subnav-toggle');
+			subnavToggle.addEventListener('click', (e) => {
+				this.toggleSubnav(e.target);
+			});
 		});
 	}
 }
