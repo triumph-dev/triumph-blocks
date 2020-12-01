d0c5c9ac964adf49eb5f4c630664dde64ef7b0d2
Andrew Hale <hale.adh@gmail.com>
2020-11-30 18:09:39 -0600

    Update Modal

--- templates/blocks/modal/modal.js
+++ templates/blocks/modal/modal.js
@@ -1,24 +1,23 @@
 export default class Modal {
-	constructor(element, externalLink) {
+	constructor(element) {
 		this.id = element.id;
 		this.modalWrapper = element;
 		this.triggers = document.querySelectorAll('.trigger-modal-' + this.id);
 		this.modal = this.modalWrapper.querySelector('.modal');
 		this.closeButtons = this.modalWrapper.querySelectorAll('.modal-close');
-		this.externalLink = externalLink ? externalLink : false;
 		this.init();
 	}
 	init() {
 		this.triggers.forEach((trigger) => {
 			trigger.addEventListener('click', (e) => {
 				e.preventDefault();
+				console.log(e);
 
-				if (this.externalLink) {
+				if (trigger.classList.contains('trigger-modal-external-link')) {
 					this.modal.querySelector('.external-link').href = e.target.href;
 				}
 
-				var modalWrapper = document.querySelector(this.id);
-				if (modalWrapper.dataset.open != 'true') {
+				if (this.modalWrapper.dataset.open != 'true') {
 					this.open();
 				}
 			});
